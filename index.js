b9d87b2c0647d7d2fb0cbf5bd680072326c95b60
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-11 12:56:30 -0400

    Solution for part two

--- index.js
+++ index.js
@@ -20,4 +20,47 @@ try{
 //PART TWO: Trampolines
 // Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
 // Once your recursive function is complete, trampoline it.
+let nestedArray = [[1,5],[4,[5],6],[7,8,[9]]];
+
+const flatArray = (arr) => {
+    let newArr = [];
+    
+    for(let i = 0; i < arr.length; i++ ){
+        if(Array.isArray(arr[i])){
+            newArr = newArr.concat(flatArray(arr[i]))
+        }
+        else{
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+}
+
+console.log(flatArray(nestedArray))
+
+const flatArrTwo = (arr, result = []) =>{
+    if(!Array.isArray(arr)){
+        return result.concat(arr)
+    }
+
+    for(let i = 0; i< arr.length; i++){
+        if(Array.isArray(arr[i])){
+           return () => flatArrTwo(arr[i], result)
+        }else{
+            result.push(arr[i])
+        }
+    }
+
+    return result;
+}
+
+const trampoline = (f, ...args) => {
+    let result = f(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  }
+
+console.log(trampoline(flatArrTwo, nestedArray))
 
