0133f611b193f8c5535296fb68571054fd3f39ef
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-12 16:49:32 -0400

    Update addPrimeNumbers so it return the sum of all prime numbers

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ Create a simple function that increments the variable, and then calls itself rec
 Surround the initial function call in a try/catch block.
 Within the catch, log the error and the value of the counter variable.*/
 
-// let counter = 0;
-// function increment(){
-//     counter++;
-//     return increment()
-// }
+let counter = 0;
+function increment(){
+    counter++;
+    return increment()
+}
 
-// try{
-//     increment()
-// }catch(e) {
-//     console.log(`Error: ${e}`)
-//     console.log(`The value of the counter is ${counter}`)
-// }
+try{
+    increment()
+}catch(e) {
+    console.log(`Error: ${e}`)
+    console.log(`The value of the counter is ${counter}`)
+}
 
 
 // //PART TWO: Trampolines
 // // Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
 // // Once your recursive function is complete, trampoline it.
-let nestedArray = [[1,5],[4,[5],6],[7,8,[9]]];
+ let nestedArray = [[1,5],[4,[5],6],[7,8,[9]]];
 
 const flatArray = (arr) => {
     let newArr = [];
     
     for(let i = 0; i < arr.length; i++ ){
         if(Array.isArray(arr[i])){
-            newArr = newArr.concat(flatArray(arr[i]))
+            newArr.push(...flatArray(arr[i]))
         }
         else{
             newArr.push(arr[i])
@@ -39,7 +39,7 @@ const flatArray = (arr) => {
 console.log(flatArray(nestedArray))
 
 const flatArrTwo = (arr, result = []) =>{
-    if(!Array.isArray(arr)){
+    if( typeof arr === 'number' || typeof arr === 'string'){
         return result.push(arr)
     }
     else{
@@ -69,40 +69,44 @@ console.log(trampoline(flatArrTwo, nestedArray))
 /*Create a simple HTML element to hold text. Cache this HTML element into a JavaScript variable.
 Once complete, use the alert() method to alert the user that the calculation is finished*/
 
-// let holdNum = document.querySelector(".primeNum");
-// console.log(holdNum)
+let holdNum = document.querySelector(".primeNumDiv");
 
-// let num = 8;
-  
-// const isPrime = () =>{
+const isPrime = (num) =>{
 
-//     let isPrimeNum = true;
+    if(num <= 1) return false;
 
-//     if(num >  1){
-//         for(let i = 2; i*i < num; i++){
-//             if(num%i === 0){
-//                 isPrimeNum = false;
-//             }
-//         }
+    //Perform a for loop if a number is greater than two
+    if(num >= 2){
+        for(let i = 2; i < num; i++){
+            if(num%i === 0){
+                return false;
+                }
+        }l
+    }
+    return true;
+}
 
-//     }
+// //Write a function that takes a parameter n and adds a list of all prime numbers between one and n to your HTML element.
 
-//     return num;
-// }
+const addPrimerNumbers = (n, count = 1, sum = 0) =>{
 
-// console.log(isPrime(num))
+    if( count > n){
+        window.alert(`Calculation is finished. The sum of all prime number is ${sum}`)
+        return;
+    }
 
-// // //Write a function that takes a parameter n and adds a list of all prime numbers between one and n to your HTML element.
+    if(isPrime(count)){
+        sum = sum + count;
+        holdNum.innerHTML += `<h1>${count} </h1> <br/>`
+    }
 
-// let n = 10;
 
-// const addPrimerNumbers = (n, count = 1) =>{
+    setTimeout(()=> addPrimerNumbers(n, count + 1, sum),0)
 
-//     if( count > n){
-//         window.alert("Calculation is finished")
-//     }
 
-//     holdNum.innerHTML += `${isPrime(num)}`
-// }
+}
 
-// addPrimerNumbers(n)
+
+let n = 50;
+addPrimerNumbers(n);
+
